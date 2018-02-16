000b6e91ce87f935ce947fa76b99ec0a8d2d1b31
paulakedouk <paulakedouk@gmail.com>
2018-02-15 21:43:02 -0800

    Twitter part done

--- liri.js
+++ liri.js
@@ -1,62 +1,36 @@
 // Hidden API keys
 require('dotenv').config();
+var inquirer = require('inquirer');
+var getTweets = require('./twitter');
 
-// required to import and store it in a variable
-var keys = require('./keys.js');
-var request = require('request');
-var Twitter = require('twitter');
-var Spotify = require('spotify');
-
-var client = new Twitter(keys.twitter);
-var spotify = new Spotify(keys.spotify);
-
-var userInput = process.argv;
-
-if (userInput[2] === 'my-tweets') {
-  getTweets();
-  //create a function to get tweets
-} else if (userInput[2] === 'spotify-this-song') {
-  console.log('spotify-this-song');
-} else if (userInput[2] === 'movie-this') {
-  console.log('movie-this');
-} else if (userInput[2] === 'do-what-it-says') {
-  console.log('do-what-it-says');
-} else {
-  console.log('try again');
-}
-
-function getTweets() {
-  var handle = '';
-
-  if (userInput.length > 4) {
-    var queryArr = [];
-    for (var i = 3; i < userInput.length; i++) {
-      queryArr.push(userInput[i]);
-      handle = queryArr.join('');
-      console.log(handle);
-    }
-  } else {
-    handle += userInput[3];
-  }
-
-  var params = { screen_name: userInput[3] };
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      if (tweets.length > 20) {
-        for (var i = 0; i < 20; i++) {
-          console.log(tweets[i].created_at);
-          console.log(tweets[i].text);
-        }
-      } else if (tweets.length === 0) {
-        console.log('There is no tweets to show you!');
-      } else {
-        for (var i = 0; i < tweets.length; i++) {
-          console.log(tweets[i].created_at);
-          console.log(tweets[i].text);
-        }
+var runApp = function() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'input',
+        message: 'Choose one of these:',
+        choices: ['my-tweets', 'spotify-this-song', 'do-what-it-says', 'movie-this']
       }
-    } else {
-      console.log('Sorry, please try again!');
+    ])
+    .then(function(value) {
+      console.log(value.input);
+
+      if (value.input === 'my-tweets') {
+        getTweets(runApp);
+      }
+    });
+};
+runApp();
+
+function getSong() {
+  spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    console.log(data);
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
     }
+
+    // Do something with 'data'
   });
 }
