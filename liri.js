39618e79b8afcc117f28f9ce6594c2e7711b4092
paulakedouk <paulakedouk@gmail.com>
2018-02-16 00:16:56 -0800

    Add log-txt to log every tweets

--- liri.js
+++ liri.js
@@ -4,22 +4,22 @@ var getTweets = require('./twitter');
 var getSpotify = require('./spotify');
 var getMovie = require('./omdb');
 var getRandom = require('./random');
+var fs = require('fs');
 
 var userInput = process.argv;
 
 var runApp = function() {
   if (userInput[2] === 'my-tweets') {
-    // console.log('my-tweets');
     getTweets(runApp);
   } else if (userInput[2] === 'spotify-this-song') {
-    // console.log('spotify-this-song');
     getSpotify(runApp);
   } else if (userInput[2] === 'movie-this') {
     getMovie(runApp);
   } else if (userInput[2] === 'do-what-it-says') {
     getRandom(runApp);
   } else {
-    console.log('try again');
+    console.log('Please choose a LIRI command.');
   }
 };
+
 runApp();
--- twitter.js
+++ twitter.js
@@ -1,7 +1,7 @@
-// required to import and store it in a variable
 var keys = require('./keys.js');
 var request = require('request');
 var Twitter = require('twitter');
+var fs = require('fs');
 
 var client = new Twitter(keys.twitter);
 
@@ -15,7 +15,7 @@ function getTweets() {
     for (var i = 3; i < userInput.length; i++) {
       queryArr.push(userInput[i]);
       handle = queryArr.join('');
-      //   console.log(handle);
+      console.log(handle);
     }
   } else {
     handle += userInput[3];
@@ -26,20 +26,37 @@ function getTweets() {
     if (!error) {
       if (tweets.length > 20) {
         for (var i = 0; i < 20; i++) {
-          console.log(tweets[i].created_at);
-          console.log(tweets[i].text);
+          var date = tweets[i].created_at;
+          var tweet = tweets[i].text;
+          console.log('____________________');
+          console.log(date + '\n');
+          console.log(tweet + '\n');
+          log(tweet);
         }
       } else if (tweets.length === 0) {
         console.log('There is no tweets to show you!');
       } else {
         for (var i = 0; i < tweets.length; i++) {
-          console.log(tweets[i].created_at);
-          console.log(tweets[i].text);
+          var date = tweets[i].created_at;
+          var tweet = tweets[i].text;
+          console.log('____________________');
+          console.log(date + '\n');
+          console.log(tweet + '\n');
+          log(tweet);
         }
       }
     } else {
       console.log('Sorry, please try again!');
     }
+  });
+}
+
+function log(tweet) {
+  fs.appendFile('log.txt', tweet, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(tweet + '\n');
   });
 }
 
