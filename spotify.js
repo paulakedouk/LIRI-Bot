7bf6ca582603dea1b0b3de5e5daf2a629be4d142
paulakedouk <paulakedouk@gmail.com>
2018-02-15 22:57:37 -0800

    Show song info

--- spotify.js
+++ spotify.js
@@ -7,19 +7,33 @@ var userInput = process.argv;
 var song;
 
 function getSpotify() {
-  //   if (userInput[3] === '') {
   song = userInput[3];
-  spotify
-    .search({ type: 'track', query: song })
-    .then(function(response) {
-      console.log('Artist: ', response.tracks.items[0].artists[0].name);
-      console.log('Song: ', response.tracks.items[0].name);
-      console.log('A preview link: ', response.tracks.items[0].external_urls.spotify);
-      console.log('Album: ', response.tracks.items[0].album.name);
-    })
-    .catch(function(err) {
-      return console.log("Spotify can't find this song, please try another one!");
-    });
+  if (song === undefined) {
+    spotify
+      .search({ type: 'track', query: 'Ace of Base' })
+      .then(function(response) {
+        songInfo(response);
+      })
+      .catch(function(err) {
+        return console.log("Spotify can't find this song, please try another one!");
+      });
+  } else {
+    spotify
+      .search({ type: 'track', query: song })
+      .then(function(response) {
+        songInfo(response);
+      })
+      .catch(function(err) {
+        return console.log("Spotify can't find this song, please try another one!");
+      });
+  }
+}
+
+function songInfo(response) {
+  console.log('Artist: ', response.tracks.items[0].artists[0].name);
+  console.log('Song: ', response.tracks.items[0].name);
+  console.log('A preview link: ', response.tracks.items[0].external_urls.spotify);
+  console.log('Album: ', response.tracks.items[0].album.name);
 }
 
 module.exports = getSpotify;
